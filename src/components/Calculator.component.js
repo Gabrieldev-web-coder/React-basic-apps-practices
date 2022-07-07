1325ca7bbd3633d31a5f9e5e79527a7a351887ab
Gabriel Monsalve <gabrielgaymer04@gmail.com>
2022-07-07 16:21:21 -0500

    salva

--- src/components/Calculator.component.js
+++ src/components/Calculator.component.js
@@ -1,5 +1,63 @@
+import '../styles/calculator.css';
+
 export default function Calculator(){
     return(
-        <h1>Calculator component</h1>
+        <div className='background'>
+        <main className='main-calculator'>
+            <div className='shape-calculator'>
+
+                <div className='actions-theme-container'>
+                    <strong className='header-text'><span>calc</span></strong>
+                    <div className='themes-class'>
+                        <strong className='theme-text'><span>THEME</span></strong>
+
+                        <div className='slider-container'>
+                            <div className='theme-selection'>
+                                <span className='selection-option first'>1</span>
+                                <span className='selection-option'>2</span>
+                                <span className='selection-option three'>3</span>
+                            </div>
+                        <div className='slider'>    
+                            <div className='point-selection'></div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='visualizer-calculator'>
+                    <output className='output-result' id='result-visualizer'></output>
+                </div>
+
+                <div className='dashboard-table-actions'>
+                    <div className='buttons-sections'>
+                        <div className='principal-numbers'>
+                            <button className='button-calc'>9</button>
+                            <button className='button-calc'>8</button>
+                            <button className='button-calc'>7</button>
+                            <button className='button-calc'>6</button>
+                            <button className='button-calc'>5</button>
+                            <button className='button-calc'>4</button>
+                            <button className='button-calc'>3</button>
+                            <button className='button-calc'>2</button>
+                            <button className='button-calc'>1</button>
+                        </div>
+                        <button className='button-calc'>.</button>
+                        <button className='button-calc'>0</button>
+                        <button className='button-calc'>/</button>
+                        <button className='button-calc'>x</button>
+                        <button className='button-calc'>-</button>
+                        <button className='button-calc'>+</button>
+                        <button className='button special-action'><strong>DEL</strong></button>
+                        <div className='base-buttons'>
+                            <button className='button special-action'><strong>RESET</strong></button>
+                            <button className='button special-action equal' ><strong>=</strong></button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </main>
+        </div>
     );
 }
--- src/components/Comments.component.js
+++ src/components/Comments.component.js
@@ -3,32 +3,32 @@ import "../styles/comments.css";
 export default function CommentsComponent(myProp){
     return(
             <div className="main-content">
-              <h1 class="main-header">Here is what our alumni say about freeCodeCamp:</h1>
+              <h1 className="main-header">Here is what our alumni say about freeCodeCamp:</h1>
               {Array.from(myProp.myProp).map( ({ testiname, company,position,text,urlPic,location }) => (
             
-            <main className="main-content">
+            <main key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="main-content">
 
-              <article className="main-testimonial-card">
+              <article key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="main-testimonial-card">
 
-                  <figure className="testimonial-image">
-                      <img className="testimonial-avatar" src={urlPic} alt={testiname}></img>
+                  <figure key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="testimonial-image">
+                      <img key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="testimonial-avatar" src={urlPic} alt={testiname}></img>
                   </figure>
 
-                  <section className="testimonial-info">
+                  <section key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="testimonial-info">
 
-                      <div className="testimonial-identify">
+                      <div key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="testimonial-identify">
 
-                          <div className="testimonial-name-location">
-                              <p> <strong><em>{testiname}</em></strong> in <cite>{location}</cite></p> 
+                          <div key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="testimonial-name-location">
+                              <p key={Math.floor(Math.random() * (100 - 1 + 1)) + 1}> <strong><em>{testiname}</em></strong> in <cite>{location}</cite></p> 
                           </div>
 
-                          <div className="testimonial-position-company">
-                              <p>{position} at <strong><em>{company}</em></strong></p>
+                          <div key={Math.floor(Math.random() * (100 - 1 + 1)) + 1} className="testimonial-position-company">
+                              <p key={Math.floor(Math.random() * (100 - 1 + 1)) + 1}>{position} at <strong><em>{company}</em></strong></p>
                           </div>
 
                       </div>
                       
-                      <p>{text}</p>
+                      <p key={Math.floor(Math.random() * (100 - 1 + 1)) + 1}>{text}</p>
                   </section>
 
               </article>
